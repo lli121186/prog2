2ebb3d14b07f1199b0abfe0bd73c48dfe7ac0889
lli211186 <lli121186@stud.gibb.ch>
2024-10-18 10:11:25 +0200

    inital commit

---
+++ prog2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace prog2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
