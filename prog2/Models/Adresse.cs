1f4016ea6de07ef263a970e505b51c5966f12806
Dominic Munz <dmu110930@stud.gibb.ch>
2024-10-29 20:42:21 +0100

    inital repository setup

---
+++ prog2/Models/Adresse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace prog2.Models
+{
+    public class Adresse
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Firma { get; set; }
+        public string Strasse { get; set; }
+        public int PLZ { get; set; }
+        public int OrtschaftId { get; set; }
+        public Ortschaft Ortschaft { get; set; }
+    }
+}
+
---
+++ prog2/Repositories/AdresseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace prog2.Repositories
+{
+    public class AdresseRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AdresseRepository(AppDbContext context) { _context = context; }
+        public async Task<List<Adresse>> GetAllAsync()
+        {
+            return await _context.Adressen.Include(a => a.Ortschaft).ToListAsync();
+        }
+
+        public async Task AddAsync(Adresse adresse)
+        {
+            _context.Adressen.Add(adresse);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
